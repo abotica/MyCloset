1966fdb7bbfbd4b011f95b873b941a9a45817af2
abotica <andrija.botica1613@icloud.com>
2024-04-07 14:00:44 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ItemsInput from './components/ItemsInput/ItemsInput'
+
+function App() {
+
+  return (
+    <>
+    <ItemsInput></ItemsInput>
+    </>
+  )
+}
+
+export default App
